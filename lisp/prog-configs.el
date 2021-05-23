09e8066d72007512b26cc1a47c3044efd70a8bf0
Leslie Ying <ying.rui@outlook.com>
2021-05-23 01:05:15 +0100

    lazy load benchmark

--- lisp/prog-configs.el
+++ lisp/prog-configs.el
@@ -2,6 +2,7 @@
 (setq use-package-always-ensure t)
 
 (use-package benchmark-init
+  :defer t
   :config
   ;; To disable collection of benchmark data after init is done.
   (add-hook 'after-init-hook 'benchmark-init/deactivate))
