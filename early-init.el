0260ff7e0f45e75288d66cf3d111191f7c931b50
Rui Ying <rui.ying@bristol.ac.uk>
2021-06-01 16:51:07 +0100

    Modify frame parameter in early-init.el

--- early-init.el
+++ early-init.el
@@ -4,14 +4,12 @@
 (setq gc-cons-threshold most-positive-fixnum)
 
 ;;frame setting
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
 (setq default-frame-alist
       (append (list
 	       '(min-height . 1)  '(height     . 45)
 	       '(min-width  . 1) '(width      . 81)
                '(vertical-scroll-bars . nil)
-               '(internal-border-width . 24)
+               '(internal-border-width . 20)
                '(left-fringe    . 0)
                '(right-fringe   . 0)
                '(tool-bar-lines . 0)
