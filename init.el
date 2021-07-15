39a76c5c040e3b262c762db63ca8404e9f0e4c48
Rui Ying (Mac) <ying.rui@outlook.com>
2021-07-15 13:52:33 +0100

    Add .gitignore file and fix perl warning in almond

--- init.el
+++ init.el
@@ -109,6 +109,12 @@
 ;;; EDITOR SECTION
 
 ;;Coding system
+;; In some old machines, you might need specify these in .bashrc
+;; export LAGNUAGE=en_US.UTF-8
+;; export LANG=en_US.UTF-8
+;; export LC_ALL=en_US.UTF-8
+;; export LC_CTYPE=en_US.UTF-8
+
 (defvar default-buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
