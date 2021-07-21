f94315cd0a72a1b3268ca23601bacf666e2008d8
Rui Ying (Mac) <ying.rui@outlook.com>
2021-07-22 00:47:43 +0100

    Add mini-frame and consult but not enabled

--- init.el
+++ init.el
@@ -599,10 +599,120 @@
   ("C-c j" . counsel-git-grep)
   ("C-c g" . counsel-git) ;;find file in current git directory
   ("C-c l" . counsel-git-log)
-  ("C-c r" . counsel-rg) ;;rg find tex
+  ("C-c r" . counsel-rg)  ;;rg find tex
   ("C-c f" . counsel-fzf) ;;fzf find file
   ("C-c e" . counsel-flycheck)
   ("C-c C-r" . counsel-recentf))
+
+
+;; yet another search system
+;; (use-package consult
+;;   ;; Replace bindings. Lazily loaded due by `use-package'.
+;;   :bind (;; C-c bindings (mode-specific-map)
+;;          ("C-c h" . consult-history)
+;;          ("C-c m" . consult-mode-command)
+;;          ("C-c b" . consult-bookmark)
+;;          ("C-c k" . consult-kmacro)
+;;          ;; C-x bindings (ctl-x-map)
+;;          ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+;;          ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+;;          ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+;;          ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+;;          ;; Custom M-# bindings for fast register access
+;;          ("M-#" . consult-register-load)
+;;          ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+;;          ("C-M-#" . consult-register)
+;;          ;; Other custom bindings
+;;          ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+;;          ("<help> a" . consult-apropos)            ;; orig. apropos-command
+;;          ;; M-g bindings (goto-map)
+;;          ("M-g e" . consult-compile-error)
+;;          ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+;;          ("M-g g" . consult-goto-line)             ;; orig. goto-line
+;;          ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+;;          ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+;;          ("M-g m" . consult-mark)
+;;          ("M-g k" . consult-global-mark)
+;;          ("M-g i" . consult-imenu)
+;;          ("M-g I" . consult-project-imenu)
+;;          ;; M-s bindings (search-map)
+;;          ("M-s f" . consult-find)
+;;          ("M-s L" . consult-locate)
+;;          ("M-s g" . consult-grep)
+;;          ("M-s G" . consult-git-grep)
+;;          ("M-s r" . consult-ripgrep)
+;;          ("M-s l" . consult-line)
+;;          ("M-s m" . consult-multi-occur)
+;;          ("M-s k" . consult-keep-lines)
+;;          ("M-s u" . consult-focus-lines)
+;;          ;; Isearch integration
+;;          ("M-s e" . consult-isearch)
+;;          :map isearch-mode-map
+;;          ("M-e" . consult-isearch)                 ;; orig. isearch-edit-string
+;;          ("M-s e" . consult-isearch)               ;; orig. isearch-edit-string
+;;          ("M-s l" . consult-line))                 ;; needed by consult-line to detect isearch
+
+;;   ;; Enable automatic preview at point in the *Completions* buffer.
+;;   ;; This is relevant when you use the default completion UI,
+;;   ;; and not necessary for Vertico, Selectrum, etc.
+;;   :hook (completion-list-mode . consult-preview-at-point-mode)
+
+;;   ;; The :init configuration is always executed (Not lazy)
+;;   :init
+
+;;   ;; Optionally configure the register formatting. This improves the register
+;;   ;; preview for `consult-register', `consult-register-load',
+;;   ;; `consult-register-store' and the Emacs built-ins.
+;;   (setq register-preview-delay 0
+;;         register-preview-function #'consult-register-format)
+
+;;   ;; Optionally tweak the register preview window.
+;;   ;; This adds thin lines, sorting and hides the mode line of the window.
+;;   (advice-add #'register-preview :override #'consult-register-window)
+
+;;   ;; Optionally replace `completing-read-multiple' with an enhanced version.
+;;   (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
+
+;;   ;; Use Consult to select xref locations with preview
+;;   (setq xref-show-xrefs-function #'consult-xref
+;;         xref-show-definitions-function #'consult-xref)
+
+;;   ;; Configure other variables and modes in the :config section,
+;;   ;; after lazily loading the package.
+;;   :config
+
+;;   ;; Optionally configure preview. The default value
+;;   ;; is 'any, such that any key triggers the preview.
+;;   ;; (setq consult-preview-key 'any)
+;;   ;; (setq consult-preview-key (kbd "M-."))
+;;   ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+;;   ;; For some commands and buffer sources it is useful to configure the
+;;   ;; :preview-key on a per-command basis using the `consult-customize' macro.
+;;   (consult-customize
+;;    consult-theme
+;;    :preview-key '(:debounce 0.2 any)
+;;    consult-ripgrep consult-git-grep consult-grep
+;;    consult-bookmark consult-recent-file consult-xref
+;;    consult--source-file consult--source-project-file consult--source-bookmark
+;;    :preview-key (kbd "M-."))
+
+;;   ;; Optionally configure the narrowing key.
+;;   ;; Both < and C-+ work reasonably well.
+;;   (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+;;   ;; Optionally make narrowing help available in the minibuffer.
+;;   ;; You may want to use `embark-prefix-help-command' or which-key instead.
+;;   ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+
+;;   ;;;; 2. projectile.el (projectile-project-root)
+;;   (autoload 'projectile-project-root "projectile")
+;;   (setq consult-project-root-function #'projectile-project-root)
+;;   ;;;; 3. vc.el (vc-root-dir)
+;;   ;; (setq consult-project-root-function #'vc-root-dir)
+;;   ;;;; 4. locate-dominating-file
+;;   ;; (setq consult-project-root-function (lambda () (locate-dominating-file "." ".git")))
+;; )
 
 ;;sorting and filtering framework for ivy
 (use-package ivy-prescient
@@ -910,6 +1020,18 @@
 ;;Highlight current line
 (add-hook 'after-init-hook 'global-hl-line-mode)
 
+;; (use-package mini-frame
+;;   :hook
+;;   (after-init . mini-frame-mode)
+;;   :config
+;;   (setq resize-mini-frames t)
+;;   ;; for gnome shell
+;;   ;; (setq x-gtk-resize-child-frames 'resize-mode)
+;;   (custom-set-variables
+;;    '(mini-frame-show-parameters
+;;      '((top . 0.8)
+;;        (width . 1.0)))))
+
 ;; highlight cursor when scroll window
 (use-package beacon
   :straight (:type git :host github
@@ -918,9 +1040,9 @@
   (after-init . beacon-mode))
 
 ;; type-writer sound effect
-(use-package selectric-mode
-  :hook
-  (after-init . selectric-mode))
+;; (use-package selectric-mode
+;;   :hook
+;;   (after-init . selectric-mode))
 
 (use-package smart-cursor-color
   :hook
@@ -1302,8 +1424,7 @@
 (use-package ample-theme :defer t )
 (use-package eziam-theme :defer t ) ;;almost perfect light theme
 (use-package spacemacs-common :defer t :straight spacemacs-theme)
-(use-package doom-themes
-  :defer t
+(use-package doom-themes :defer t
   ;; :config
   ;; ;;treemacs setting
   ;; (setq doom-themes-treemacs-enable-variable-pitch nil)
@@ -1331,11 +1452,11 @@
 (defun day-theme ()
   "Activate a light color theme.  Recommendation: leuven, spacemacs-light, eziam, twilight-bright."
   (interactive)
-  (setq custom-enabled-themes '(modus-vivendi))
+  (setq custom-enabled-themes '(modus-operandi))
   (reapply-themes))
 
 (defun night-theme ()
-  "Activate a dark color theme.  Recommendation: humanoid-dark, doom-one, doom-dark+, ample-flat."
+  "Activate a dark color theme.  Recommendation: humanoid-dark, doom-one/vibrant, doom-dark+, ample-flat."
   (interactive)
   (setq custom-enabled-themes '(doom-one))
   (reapply-themes))
