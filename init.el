80e2239366ef3172632169b55cc9f65c8baf8b13
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-10-17 21:23:23 +0100

    add auctex and bookmark

--- init.el
+++ init.el
@@ -1243,6 +1243,55 @@
 (use-package cmake-mode
   :mode ("\\.cmake\\'" "CMakeLists\\.txt\\'"))
 
+;;;;;;;;;;;;;;
+;; LaTeX    ;;
+;;;;;;;;;;;;;;
+
+;; AUCTEX
+;; all in one: C-c C-c
+(use-package auctex
+  :ensure t
+  :no-require t
+  :config
+  ;; get support for many of the LaTeX packages in your documents
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+
+  ;; pdf viewer: Skim
+  (setq TeX-view-program-list
+	'(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+
+  (setq TeX-view-program-selection '((output-pdf "Skim")))
+  ;; Forward search happens automatically upon calling the viewer,
+  (setq TeX-source-correlate-method 'synctex)
+  :hook
+  ;; SyncTeX setup
+  (LaTeX-mode . TeX-source-correlate-mode)
+  :bind
+  (:map LaTeX-mode-map
+	("C-c C-a" . TeX-command-run-all)
+	("M-<mouse-1>" . TeX-view)))
+
+;; Enable RefTeX for managing cross-references, citations, and labels
+;; (use-package reftex
+;;   :commands (turn-on-reftex)
+;;   :init
+;;   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+;;   (setq reftex-plug-into-AUCTeX t))
+
+;; for fast insertion symbols
+;; C-c $, or ` to insert math symbol
+;; (use-package cdlatex
+;;   :hook
+;;   (LaTeX-mode . turn-on-cdlatex))
+
+;; bookmark is for quickly jumping to a file/location
+(use-package bookmark
+  :defer t
+  :straight (:type built-in)
+  :config
+  (bookmark-bmenu-list))
+
 
 ;;; Text-mode: Markdown/org-mode/TeX
 
