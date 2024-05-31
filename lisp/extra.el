f81abd5c587f360e5146a53ae0b2753f9c8cf434
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2024-05-31 21:11:53 +0100

    add rime input method back in extra.el

--- lisp/extra.el
+++ lisp/extra.el
@@ -1,39 +1,39 @@
-(use-package kind-icon
-  :after corfu
-  :custom
-  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+;; (use-package kind-icon
+;;   :after corfu
+;;   :custom
+;;   (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
+;;   :config
+;;   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
-(use-package nerd-icons
-  :custom
-  (nerd-icons-font-family myfont)
-  (nerd-icons-scale-factor 1.5))
+;; (use-package nerd-icons
+;;   :custom
+;;   (nerd-icons-font-family myfont)
+;;   (nerd-icons-scale-factor 1.5))
 
-(use-package nerd-icons-dired
-  :if window-system
-  :hook
-  (dired-mode . nerd-icons-dired-mode))
+;; (use-package nerd-icons-dired
+;;   :if window-system
+;;   :hook
+;;   (dired-mode . nerd-icons-dired-mode))
 
-(use-package nerd-icons-completion
-  :config
-  (nerd-icons-completion-mode))
+;; (use-package nerd-icons-completion
+;;   :config
+;;   (nerd-icons-completion-mode))
 
-(use-package nerd-icons-ibuffer
-  :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
-  :config
-  (setq nerd-icons-ibuffer-icon t))
+;; (use-package nerd-icons-ibuffer
+;;   :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
+;;   :config
+;;   (setq nerd-icons-ibuffer-icon t))
 
-;; highlight cursor when scroll window
-(use-package beacon
-  :straight (:host github :repo "Malabarba/beacon")
-  :hook
-  (after-init . beacon-mode))
+;; ;; highlight cursor when scroll window
+;; (use-package beacon
+;;   :straight (:host github :repo "Malabarba/beacon")
+;;   :hook
+;;   (after-init . beacon-mode))
 
-;; Change cursor color dynamically at cursor or pointer
-(use-package smart-cursor-color
-  :hook
-  (after-init . smart-cursor-color-mode))
+;; ;; Change cursor color dynamically at cursor or pointer
+;; (use-package smart-cursor-color
+;;   :hook
+;;   (after-init . smart-cursor-color-mode))
 
 ;; Enable this to get a superior terminal emulator (a true application like iTerm)
 ;; read more on https://github.com/akermu/emacs-libvterm to see the external dependencies
@@ -66,5 +66,15 @@
   (setq easy-hugo-default-ext ".org")
   (setq easy-hugo-org-header nil)
   )
+
+(use-package rime
+    :custom
+    (default-input-method "rime")
+    ;; Path to the Emacs dynamic module
+    (rime-emacs-module-header-root "/opt/homebrew/Cellar/emacs-mac/emacs-29.1-mac-10.0/include")
+    ;; Path to the librime library
+    (rime-librime-root "~/.emacs.d/librime/dist")
+    ;; Path to the rime configuration file
+    (rime-user-data-dir "~/Library/rime"))
 
 (provide 'extra)
